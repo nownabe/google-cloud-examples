a7e507c2a778e3598e013e2adf383626fd7ccfaa
nownabe <nownabe@users.noreply.github.com>
2023-03-01 17:06:35 +0900

    Add terraform

---
+++ python/vertexai-matching-engine/terraform/startup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo 'PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION="python"' >> /etc/environment
+
+apt update
+apt -y install python3-pip git
+pip3 install tensorflow google-cloud-aiplatform
+
+gsutil -m cp -r gs://cloud-samples-data/ai-platform/flowers /opt/
+
+git clone https://github.com/nownabe/google-cloud-examples /opt/google-cloud-examples
