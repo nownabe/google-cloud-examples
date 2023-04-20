640c4622ad63ec8333b9a7ae701c049627c6fa4d
nownabe <nownabe@users.noreply.github.com>
2023-04-20 20:48:33 +0900

    Add generate_and_upload_embeddings.py

---
+++ python/getting-started-matching-engine/generate_and_upload_embeddings.py
@@ -0,0 +1,64 @@
+import json
+import os
+import sys
+from tempfile import NamedTemporaryFile
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+import numpy as np
+import tensorflow as tf
+from google.cloud import storage
+
+
+BUCKET = "cloud-samples-data"
+PREFIX = "ai-platform/flowers/"
+
+tf.keras.utils.disable_interactive_logging()
+model = tf.keras.applications.EfficientNetB0(include_top=False, pooling="avg")
+
+
+def blob_to_embedding(blob: storage.Blob) -> list[float]:
+    with NamedTemporaryFile(prefix="flowers") as temp:
+        blob.download_to_filename(temp.name)
+        raw = tf.io.read_file(temp.name)
+
+    image = tf.image.decode_jpeg(raw, channels=3)
+    prediction = model.predict(np.array([image.numpy()]))
+    return prediction[0].tolist()
+
+
+def generate_and_upload_embeddings(flower: str, destination_root: str) -> None:
+    print(f"Started generating and uploading embeddings for {flower}")
+
+    client = storage.Client(project="gs-matching-engine")
+
+    datapoints = []
+
+    blobs = list(client.list_blobs(BUCKET, prefix=f"{PREFIX}{flower}/"))
+    for i, blob in enumerate(blobs, 1):
+        print(f"[{i:3d}/{len(blobs)}] Processing {blob.name}")
+
+        embedding = blob_to_embedding(blob)
+        datapoints.append({
+            "id": blob.name,
+            "embedding": embedding,
+        })
+
+    dst_bucket_name, dst_base = destination_root[5:].split("/", maxsplit=1)
+    dst_bucket = client.bucket(dst_bucket_name)
+    dst_blob = dst_bucket.blob(os.path.join(dst_base, flower + ".json"))
+
+    with dst_blob.open(mode="w") as f:
+        for datapoint in datapoints:
+            f.write(json.dumps(datapoint) + "\n")
+
+    print(f"Finished generating and uploading embeddings for {flower} 🎉")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python generate_and_upload_embeddings.py FLOWER DESTINATION_ROOT")
+        print("  FLOWER: daisy, dandelion, roses, sunflowers, or tulips")
+        print("  DESTINATION_ROOT: root path of Cloud Storage like gs://my-bucket/embeddings/")
+    else:
+        generate_and_upload_embeddings(sys.argv[1], sys.argv[2])
